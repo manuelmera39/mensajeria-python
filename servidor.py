7446e6d13f4f1bb4c670aa6d501c3f7f6b3d7199
manuelmera39 <manuelmera39@gmail.com>
2026-03-18 18:48:17 +0100

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -116,6 +116,10 @@ def lecturas(nombre):
 # -------------------------------
 # ARRANQUE
 # -------------------------------
+import os
+
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
     print("Servidor listo")
-    app.run()
+    app.run(host="0.0.0.0", port=port)
